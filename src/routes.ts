b0c9d3b258615ef9f2f7721103fbddc7dbeb3e8a
franzinBr <alan-franzin2011@live.com>
2021-06-23 13:23:38 -0400

    added tag route

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,15 @@
 import { Router } from "express";
+import { CreateTagController } from "./controllers/CreateTagController";
 import { CreateUserController } from "./controllers/CreateUserController";
+import { ensureAdmin } from "./middlewares/ensureAdmin";
+
 const router = Router();
 
 const createUserController = new CreateUserController();
-router.post("/users/", createUserController.handle);
+const createTagController = new CreateTagController();
+
+
+router.post("/users", createUserController.handle);
+router.post("/tags", ensureAdmin ,createTagController.handle);
 
 export { router }

07d5a434dc4bf6af57e720a1f1d66d56d327bfe2
franzinBr <alan-franzin2011@live.com>
2021-06-25 23:17:24 -0400

    Init styles

---
+++ client/src/styles/global.ts
@@ -0,0 +1,66 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    html{
+        font-size: 100%;
+    }
+
+    body {
+        background: ${props => props.theme.colors.background};
+        color: ${props => props.theme.colors.text};
+        font: 400 1rem Roboto, sans-serif;
+    }
+
+    a {
+        text-decoration: none;
+        color: ${props => props.theme.colors.text};
+    }
+
+    button {
+        border: none;
+    }
+
+    Input{
+        outline: none;
+    }
+
+    .MainContainer {
+        display: flex;
+        //align-items: center;
+        justify-content: center;
+
+        max-width: 50rem;
+        margin: 0 auto;
+        margin-top: .1rem;
+        background: #FFFFFF;
+        height: calc(100vh - 4.1rem);
+        -ms-overflow-style: none; /* for Internet Explorer, Edge */
+        scrollbar-width: none; /* for Firefox */
+        overflow-y: scroll; 
+    }
+    .MainContainer::-webkit-scrollbar {
+        display: none; /* for Chrome, Safari, and Opera */
+    }
+
+
+    @media (max-width: 768px){
+        html {
+            font-size: 80%;
+        }
+        .MainContainer {
+            width: 100vw;
+        }
+    }
+
+    @media (max-width: 430px){
+        html {
+            font-size: 75%;
+        }
+    }
+`;
---
+++ client/src/styles/theme.ts
@@ -0,0 +1,11 @@
+
+const theme = {
+    colors: {
+      background: '#FAFAFA',
+      text: '#262626',
+      text_second: "#787680",
+      primary: '#fbc531'
+    }
+  }
+  
+export default theme

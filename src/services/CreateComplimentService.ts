31c937282abdab478cbfd01a31b29a348bb2dbd0
franzinBr <alan-franzin2011@live.com>
2021-06-26 15:52:07 -0400

    send an email to a person praised

--- src/services/CreateComplimentService.ts
+++ src/services/CreateComplimentService.ts
@@ -1,6 +1,8 @@
 import { getCustomRepository } from "typeorm"
 import { ComplimentsRepositories } from "../repositories/ComplimentsRepositories"
 import { UsersRepositories } from "../repositories/UsersRepositories";
+import { EmailSender } from "../utils/emailSender";
+import { ErrorResponse } from "../utils/ErrorResponse";
 
 interface iComplimentRequest{
     tag_id: string,
@@ -13,13 +15,13 @@ class CreateComplimentService {
 
     async execute({tag_id, user_sender, user_receiver, message}: iComplimentRequest){
         
-        if(user_sender === user_receiver) throw new Error("Impossible to create self-praise")
+        if(user_sender === user_receiver) throw new ErrorResponse("Impossible to create self-praise", 400)
         
         const complimentsRepositories = getCustomRepository(ComplimentsRepositories);
         const usersRepositories = getCustomRepository(UsersRepositories)
 
         const userReceiverExists = await usersRepositories.findOne(user_receiver)
-        if(!userReceiverExists) throw new Error("User Receiver doesn't exists!");
+        if(!userReceiverExists) throw new ErrorResponse("User Receiver doesn't exists!", 404);
 
         const compliment = complimentsRepositories.create({
             tag_id,
@@ -29,6 +31,16 @@ class CreateComplimentService {
         })
 
         await complimentsRepositories.save(compliment);
+
+        const messageEmail = `
+        <h1>Hello ${userReceiverExists.name}</h1>
+        <p>You just received a compliment, check your profile</p>
+        `
+        EmailSender.send({
+            to: userReceiverExists.email,
+            subject: "Compliment",
+            text: messageEmail,
+        })
 
         return compliment;
 

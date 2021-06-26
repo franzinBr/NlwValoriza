13c93681da1fd8d6b994341c28cbd7dbd449ceac
franzinBr <alan-franzin2011@live.com>
2021-06-26 15:59:58 -0400

    implementing compliment creation

--- client/src/components/CreateCompliment.tsx
+++ client/src/components/CreateCompliment.tsx
@@ -4,6 +4,9 @@ import { DivCreateCompliment } from '../styles/components/CreateCompliments';
 
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { createComplimentRequest } from "../services/compliments";
+import { ICompliment } from "../Interfaces/ComplimentInterface";
+import { useContext } from 'react';
+import { AuthContext } from '../contexts/AuthContext';
 
 interface Tag {
     id: string,
@@ -16,6 +19,7 @@ interface Tag {
 interface Tags{
     tags: Tag[];
     id: string;
+    setNewCompliments?: React.Dispatch<React.SetStateAction<ICompliment[]>>
 }
 
 type Inputs = {
@@ -23,15 +27,33 @@ type Inputs = {
 };
   
 
-const CreateCompliment: React.FC<Tags> = ({tags, id}) => {
+const CreateCompliment: React.FC<Tags> = ({tags, id, setNewCompliments}) => {
     const { register, handleSubmit, watch, formState: { errors }, reset } = useForm();
     const [select, setSelect] = React.useState('');
+    const {user} = useContext(AuthContext)
 
     const onSubmit: SubmitHandler<Inputs> = async data => {
-        if(data.message.length > 0 && select.length > 0)
-        {
+        if(data.message.length > 0 && select.length > 0){
             const res = await createComplimentRequest({tag_id: select, user_receiver: id, message: data.message});
-            if(res.data.success)  reset({keepValues: false})
+            if(res.data.success) {
+                reset({keepValues: false})
+
+                const newCompliment: ICompliment = {
+                    id: `new${+new Date().getTime()}`,
+                    message: data.message,
+                    userSender: {
+                        id: user.id,
+                        name: user.name,
+                        username: user.username,
+                    },
+                    tag: {
+                        id: select,
+                        name: tags.filter(tag => tag.id == select)[0].name
+                    }
+                }
+                setNewCompliments(state => [newCompliment, ...state])
+
+            } 
         }
     }
 

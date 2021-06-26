f8f125133a1942b79223d2b2573c16ca9f5d1de2
franzinBr <alan-franzin2011@live.com>
2021-06-25 23:05:19 -0400

    middleware ensure admin

--- src/middlewares/ensureAdmin.ts
+++ src/middlewares/ensureAdmin.ts
@@ -1,9 +1,15 @@
 import {Request, Response, NextFunction} from "express";
+import { getCustomRepository } from 'typeorm';
+import { UsersRepositories } from "../repositories/UsersRepositories";
 
 
-export function ensureAdmin(req: Request, res: Response, next: NextFunction ) {
+export async function ensureAdmin(req: Request, res: Response, next: NextFunction ) {
+    
+    const { user_id } = req;
+
+    const usersRepositories = getCustomRepository(UsersRepositories)
+    const { admin } = await usersRepositories.findOne(user_id)
     // Verify if users is admin
-    const admin = true;
     if(admin) return next();
 
     return res.status(401).json({

7210392bc58a86ace16ab15bc59e5dd4a4811fee
franzinBr <alan-franzin2011@live.com>
2021-06-25 22:58:40 -0400

    active cors and cookies

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,17 @@
+require('dotenv').config()
 import "reflect-metadata";
 import express, {Request, Response, NextFunction} from 'express';
 import "express-async-errors";
 import {router} from './routes';
 import "./database";
+import cors from 'cors';
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 app.use(express.json())
-
+app.use(cors({ origin: process.env.FRONT_URL, credentials: true }));
+app.use(cookieParser())
 app.use(router);
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
@@ -24,6 +28,6 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     })
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}!`));
